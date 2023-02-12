c4688ac2f1c48a7b0d649de607531611c81a7b0a
ashritha <ashrithareddy1004@gmail.com>
2023-02-12 20:29:09 +0530

    added func to handle error

--- leave_mag.go
+++ leave_mag.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type student struct {
 	Name string `json:"name"`
 	Id   int32  `json:"id"`
@@ -16,4 +23,17 @@ type leave struct {
 	Status string `json:"status"`
 }
 type leave_approval struct {
+	LeaveId int32  `json:"leaveid"`
+	Status  string `json:"status"`
+}
+
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	handleError(err)
+	return os.Getenv(key)
+}
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
